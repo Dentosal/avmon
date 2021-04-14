3f0ef125586f676bdbd01b9d41951be22c9b805e
Hannes Karppila <hannes.karppila@gmail.com>
2021-04-15 01:14:37 +0300

    Dockerize

---
+++ avmon/__main__.py
@@ -0,0 +1,16 @@
+from os import environ
+import asyncio
+
+
+if __name__ == "__main__":
+    role = environ["AVMON_ROLE"]
+
+    if role == "collector":
+        from .collector import main
+    elif role == "backend":
+        from .backend import main
+    elif role == "frontend":
+        from .frontend import main
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
