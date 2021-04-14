b48ecd92f500ac67ccdeea213a9db5d50603e73e
Hannes Karppila <hannes.karppila@gmail.com>
2021-04-15 01:13:34 +0300

    Update message test to include new fields

--- tests/test_message.py
+++ tests/test_message.py
@@ -3,6 +3,12 @@ from avmon.message import EndpointStatus
 
 def test_message_conversion():
     msg = EndpointStatus(
-        url="https://example.org/", reached=True, status=200, regex_match=None
+        url="https://example.org/",
+        reached=True,
+        status=200,
+        regex_match=None,
+        error=None,
+        time_start=0,
+        time_end=1,
     )
     assert msg == EndpointStatus.from_json(msg.to_json())
