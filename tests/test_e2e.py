f2d80535ca7c7d63e7cdf7e1b887808de99ee947
Hannes Karppila <hannes.karppila@gmail.com>
2021-04-15 03:21:33 +0300

    Add full E2E test

---
+++ tests/test_e2e.py
@@ -0,0 +1,59 @@
+import pytest
+from contextlib import contextmanager
+import subprocess
+import re
+import asyncio
+import aiohttp
+
+from avmon.collector import endpoint_task
+from avmon.config import EndpointConfig
+
+
+@contextmanager
+def infra():
+    """Test with full infrastructure."""
+    subprocess.run(
+        [
+            "docker-compose",
+            "-f",
+            "docker-compose.yml",
+            "-f",
+            "tests/docker-compose.yml",
+            "up",
+            "--build",
+            "-d",
+        ],
+        check=True,
+    )
+
+    try:
+        yield
+    finally:
+        subprocess.run(["docker-compose", "down", "-v"], check=False)
+
+
+@pytest.mark.asyncio
+async def test_e2e():
+    """
+    End-to-end test using the full pipeline.
+    Uses test configuration and checks that http visualization includes correct values.
+    """
+
+    with infra():
+        await asyncio.sleep(5.0)
+        async with aiohttp.ClientSession() as session:
+            async with session.get("http://localhost:8080/", timeout=5) as response:
+                text = await response.text()
+                rows = re.findall(r"<tr.*?>(.+?)</tr>", text)
+                assert len(rows) == 3
+                for r in rows:
+                    fields = re.findall(r"<td.*?>(.*?)</td>", r)
+
+                    if "delay" in fields[0]:
+                        assert fields
+
+                    elif "/status/200" in fields[0]:
+                        assert fields[2] == "200"
+
+                    elif "/status/400" in fields[0]:
+                        assert fields[2] == "400"
