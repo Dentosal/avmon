13e27576300032ba568fc3ea17b649bfb354829e
Hannes Karppila <hannes.karppila@gmail.com>
2021-04-16 02:07:01 +0300

    Add a new test for colletor

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -1,11 +1,15 @@
+from typing import List, Optional
+
 import pytest
 
+from dataclasses import dataclass
+from contextlib import asynccontextmanager
 import portpicker
 import socket
 import asyncio
 from aiohttp import web
 
-from avmon.collector import endpoint_task
+from avmon.collector import endpoint_task, check_endpoint
 from avmon.config import EndpointConfig
 
 
@@ -60,3 +64,44 @@ async def test_endpoint_task():
         assert str(e) == "Stop"
 
     assert request_count == 1
+
+
+@dataclass
+class MockSession:
+    """Mock session that returns given responses in order."""
+
+    responses: List["MockResponse"]
+
+    @asynccontextmanager
+    async def get(self, url: str, **_ignored_kwargs):
+        yield self.responses.pop(0)
+
+
+@dataclass
+class MockResponse:
+    status: int
+    content: str
+
+    async def text(self):
+        return self.content
+
+
+@pytest.mark.asyncio
+async def test_check_endpoint():
+    cfg = EndpointConfig(
+        interval=0.000001,
+        timeout=1.0,
+        description="test",
+        url=f"THIS IS IGNORED",
+        regex=r"\ba{4}\b",  # One "word" with exactly four lowercase "a" letters
+    )
+
+    mock_session = MockSession([])
+
+    for status, word in [(200, "aaaaa"), (200, "aaaa"), (404, "nothing")]:
+        mock_session.responses.append(
+            MockResponse(status=status, content=f"Test {word} content")
+        )
+        result = await check_endpoint(mock_session, cfg)
+        assert result.status == status
+        assert result.regex_match == (word == "aaaa")
